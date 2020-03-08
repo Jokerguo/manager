ed99f15ff9ea1dfe90c35a646335ebb33cfc585f
jokerguo <1977036313@qq.com>
2020-03-08 13:24:12 +0800

    money 页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,24 @@
 <template>
-  <div>
-    Money.vue
+  <div class="nav-wrapper">
+    <div class="nav-content">
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+      <p>Money.vue</p>
+    </div>
     <Nav/>
   </div>
 </template>
@@ -12,5 +30,16 @@
 </script>
 
 <style lang="scss" scoped>
+  .nav-wrapper {
+    border: 1px solid green;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
 
+  .nav-content {
+    border: 1px solid blue;
+    flex-grow: 1;
+    overflow: auto;
+  }
 </style>
