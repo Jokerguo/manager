95e6740eb899f98ee586728eee0e6c8f3daba5e9
jokerguo <1977036313@qq.com>
2020-03-10 16:30:44 +0800

    使用v-model 代替:value @input

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -2,29 +2,34 @@
   <div>
     <label class="notes">
       <span class="name">备注</span>
-      <input type="text" placeholder="在这里添加备注">
+      <input type="text" placeholder="在这里添加备注" v-model="value">
     </label>
   </div>
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'Notes'
-  };
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+@Component
+  export default class Notes extends Vue {
+    value = '';
+  }
 </script>
 
 <style lang="scss" scoped>
-  .notes{
+  .notes {
     background: #f5f5f5;
     display: flex;
     font-size: 14px;
     padding: 0 16px;
     align-items: center;
-    > .name{
+
+    > .name {
       padding-right: 16px;
     }
 
-    input{
+    input {
       padding: 16px 0;
       flex-grow: 1;
       background: transparent;
