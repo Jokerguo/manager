0299735cc8866125d1c31154ebfea7ee475c43e3
jokerguo <1977036313@qq.com>
2020-03-10 21:04:16 +0800

    将record 放在recordList中 并添加了localstorage

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,31 +1,64 @@
 <template>
   <Layout class-prefix="layout">
-    <NumberPad/>
-    <Types/>
-    <Notes/>
-    <Tags :data-source="tag"/>
+    <NumberPad @update:value="onUpdateAmounts" @submit="saveRecord"/>
+    <Types :value.sync="record.type" />
+    <Notes @update:value="onUpdateNotes"/>
+    <Tags :data-source.sync="tag" @update:value="onUpdateTags"/>
+    {{recordList}}
   </Layout>
 </template>
 
-<script>
+<script lang="ts">
+  import Vue from 'vue';
   import NumberPad from '@/components/Money/NumberPad.vue';
   import Types from '@/components/Money/Types.vue';
   import Notes from '@/components/Money/Notes.vue';
   import Tags from '@/components/Money/Tags.vue';
-  export default {
-    name: 'Money',
+  import {Component, Watch} from 'vue-property-decorator';
+
+  type Record ={
+    tags: string[];
+    notes: string;
+    type: string;
+    amounts: number;
+  }
+  @Component({
     components: {Tags, Notes, Types, NumberPad},
-    data(){
-      return{
-        tag : ['衣','食','住','行']
-      }
+  })
+  export default class Money extends Vue {
+    tag = ['衣', '食', '住', '行'];
+    recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
+    record: Record = {
+      tags : [], notes : '', type : '-',amounts : 0
+    };
+
+    onUpdateTags(value: string[]) {
+      this.record.tags = value;
     }
 
-  };
+    onUpdateNotes(value: string) {
+      this.record.notes = value;
+    }
+
+    onUpdateAmounts(value: string){
+      this.record.amounts= parseFloat(value);
+    }
+    saveRecord(){
+      const newRecord = JSON.parse(JSON.stringify(this.record));
+       this.recordList.push(newRecord);
+    }
+
+    @Watch('recordList')
+    onRecordListChange(){
+      window.localStorage.setItem('recordList',JSON.stringify(this.recordList));
+    }
+
+
+  }
 </script>
 
 <style lang="scss">
-  .layout-content{
+  .layout-content {
     display: flex;
     flex-direction: column-reverse;
   }
