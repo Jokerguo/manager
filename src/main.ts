3b210f105b453849043a98974c69429f2132f496
jokerguo <1977036313@qq.com>
2020-03-14 21:29:19 +0800

    优化使用 保证用户用手机打开

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ new Vue({
 if(document.documentElement.clientWidth > 500){
   window.alert('请使用手机打开，浏览效果更好');
   const img =document.createElement('img');
-  img.src = '/qrcode.png';
+  img.src = './qrcode.png';
   img.style.position = 'fixed';
   img.style.top = '50%';
   img.style.left = '50%';
