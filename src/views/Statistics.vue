5d3a7eea4d2185a59fccef957364f6bc9a9fd143
jokerguo <1977036313@qq.com>
2020-03-14 11:44:25 +0800

    完成列表的css

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,11 +2,23 @@
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
     <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
+    <ol>
+      <li v-for="(group,index) in result" :key="index">
+        <h3 class="title">{{group.title}}</h3>
+        <ol>
+          <li v-for="item in group.items" :key="item.id" class="record">
+            <span>{{tagString(item.tags)}}</span>
+            <span class="notes">{{item.notes}}</span>
+            <span>￥{{item.amount}}</span>
+          </li>
+        </ol>
+      </li>
+    </ol>
   </Layout>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
+  import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
   import Tabs from '@/components/Tabs.vue';
   import intervalList from '@/constants/intervalList';
@@ -15,7 +27,32 @@
   @Component({
     components: {Tabs}
   })
-  export default class Statistics extends Vue{
+  export default class Statistics extends Vue {
+    tagString(tags: string[]){
+      console.log(tags);
+      return tags.length === 0 ? '无' : tags.join(',');
+    }
+    get recordList() {
+      return (this.$store.state as RootState).recordList;
+    }
+
+    get result() {
+      const {recordList} = this;
+      type HashTableValue = {title: string ;items: RecordItem[]}
+      const hashTable: { [key: string]: HashTableValue } = {};
+      for (let i = 0; i < recordList.length; i++) {
+        /* eslint-disable */
+        const [data, time] = recordList[i].createdAt!.split('T');
+        hashTable[data] = hashTable[data] || {title : data, items : []}; //初始化
+        hashTable[data].items.push(recordList[i]);
+      }
+      return hashTable;
+    }
+
+    created() {
+      this.$store.commit('fetchRecords');
+    }
+
     type = '-';
     interval = 'day';
     intervalList = intervalList;
@@ -24,16 +61,41 @@
 </script>
 
 <style lang="scss" scoped>
-    ::v-deep .type-item {
-      background: #fff;
-      &.selected{
-        background: #c4c4c4;
-        &::after{
-          display: none;
-        }
-      }      
+  ::v-deep {
+    .type-item {
+    background: #fff;
+
+    &.selected {
+      background: #c4c4c4;
+
+      &::after {
+        display: none;
+      }
     }
-  ::v-deep .interval-item{
-    height: 40px;
   }
+    .interval-item {
+      height: 40px;
+    }
+  }
+
+  %item{
+    padding: 0 16px;
+    min-height: 40px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .title{
+    @extend %item
+  }
+  .record{
+    @extend %item;
+    background: #fff;
+  }
+  .notes{
+    margin-right: auto;
+    margin-left: 16px;
+    color: #999;
+  }
+
 </style>
